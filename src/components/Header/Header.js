9b1c403e943d57708c3ea045356601d1c2c0480e
Leo <leofreitas.engineer@gmail.com>
2023-05-30 21:23:03 -0300

    update header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,15 +2,18 @@ import "./Header.css";
 import Links from "./Links";
 import Login from "./Login";
 
-import {BookHalf} from "react-bootstrap-icons";
+import {Github} from "react-bootstrap-icons";
 
 const Header = ({header, handleLanguageClick}) => {
     const currentPath = document.URL.split('/')[3];
     const cssColor = currentPath === "about" ? "purple" : "orange";
+    const handleIconClick = () => {
+        window.open('https://github.com/Leo3965/forum-artificial-intelligence', '_blank');
+    }
 
     return (
         <nav className="header__ai">
-            <BookHalf className="icon__ai"/>
+            <Github onClick={handleIconClick} className="icon__ai"/>
             <Links header={header} color={cssColor}/>
             <Login handleLanguageClick={handleLanguageClick} color={cssColor}/>
         </nav>
